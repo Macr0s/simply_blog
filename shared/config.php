66b469af6464f86b83a7f58a5de481306c715287
Matteo Filippi <matte0809@gmail.com>
2012-08-01 18:31:59 +0200

    File di configurazione del blog

---
+++ shared/config.php
@@ -0,0 +1,9 @@
+<?php
+	define("dropbox_url","https://www.dropbox.com/sh/q2krpx42sgfnh7k/1N2Pk5c2lx/");
+	define("dropbox_office_url","https://docs.google.com/viewer?url=https://dl.dropbox.com/sh/q2krpx42sgfnh7k/oiLasDJt3L/");
+	
+	define("db_server", "localhost");
+	define("db_user","");
+	define("db_pass","");
+	define("db_data";"");
+?>
